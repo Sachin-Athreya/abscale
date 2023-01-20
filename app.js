ee4d1e9700dbfd798d9dc6ec72462761f5a1af8f
Sachin-Athreya <110717731+Sachin-Athreya@users.noreply.github.com>
2023-01-20 16:18:51 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,306 @@
+// Text Animation Global for abscale
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Split text into spans
+  let typeSplit = new SplitType("[text-split]", {
+    types: "words, chars",
+    tagName: "span"
+  });
+
+  // Link timelines to scroll position
+  function createScrollTrigger(triggerElement, timeline) {
+    // Reset tl when scroll out of view past bottom of screen
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: "top bottom",
+      onLeaveBack: () => {
+        timeline.progress(0);
+        timeline.pause();
+      }
+    });
+    // Play tl when scrolled into view (60% from top of screen)
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: "top 60%",
+      onEnter: () => timeline.play()
+    });
+  }
+
+  $("[words-slide-up]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      yPercent: 100,
+      duration: 0.5,
+      ease: "back.out(2)",
+      stagger: { amount: 0.5 }
+    });
+    createScrollTrigger($(this), tl);
+  });
+
+  $("[words-rotate-in]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.set($(this).find(".word"), { transformPerspective: 1000 });
+    tl.from($(this).find(".word"), {
+      rotationX: -90,
+      duration: 0.6,
+      ease: "power2.out",
+      stagger: { amount: 0.1 }
+    });
+    createScrollTrigger($(this), tl);
+  });
+
+  $("[words-slide-from-right]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".word"), {
+      opacity: 0,
+      x: "1em",
+      duration: 0.6,
+      ease: "power2.out",
+      stagger: { amount: 0.2 }
+    });
+    createScrollTrigger($(this), tl);
+  });
+
+  $("[letters-slide-up]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      yPercent: 100,
+      duration: 0.2,
+      ease: "power1.out",
+      stagger: { amount: 0.6 }
+    });
+    createScrollTrigger($(this), tl);
+  });
+
+  $("[letters-slide-down]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      yPercent: -120,
+      duration: 0.3,
+      ease: "power1.out",
+      stagger: { amount: 0.7 }
+    });
+    createScrollTrigger($(this), tl);
+  });
+
+  $("[letters-fade-in]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      opacity: 0,
+      duration: 0.2,
+      ease: "power1.out",
+      stagger: { amount: 0.8 }
+    });
+    createScrollTrigger($(this), tl);
+  });
+
+  $("[letters-fade-in-random]").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), {
+      opacity: 0,
+      duration: 0.05,
+      ease: "power1.out",
+      stagger: { amount: 0.4, from: "random" }
+    });
+    createScrollTrigger($(this), tl);
+  });
+
+  $("[scrub-each-word]").each(function (index) {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: $(this),
+        start: "top 90%",
+        end: "top center",
+        scrub: true
+      }
+    });
+    tl.from($(this).find(".word"), {
+      opacity: 0.2,
+      duration: 0.2,
+      ease: "power1.out",
+      stagger: { each: 0.4 }
+    });
+  });
+
+  // Avoid flash of unstyled content
+  gsap.set("[text-split]", { opacity: 1 });
+});
+
+//Marquee Global for abscale
+// MARQUEE POWER-UP
+window.addEventListener("DOMContentLoaded", (event) => {
+  // attribute value checker
+  function attr(defaultVal, attrVal) {
+    const defaultValType = typeof defaultVal;
+    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+    if (attrVal === "true" && defaultValType === "boolean") return true;
+    if (attrVal === "false" && defaultValType === "boolean") return false;
+    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+    return defaultVal;
+  }
+  // marquee component
+  $("[tr-marquee-element='component']").each(function (index) {
+    let componentEl = $(this),
+      panelEl = componentEl.find("[tr-marquee-element='panel']"),
+      triggerHoverEl = componentEl.find("[tr-marquee-element='triggerhover']"),
+      triggerClickEl = componentEl.find("[tr-marquee-element='triggerclick']");
+    let speedSetting = attr(100, componentEl.attr("tr-marquee-speed")),
+      verticalSetting = attr(false, componentEl.attr("tr-marquee-vertical")),
+      reverseSetting = attr(false, componentEl.attr("tr-marquee-reverse")),
+      scrollDirectionSetting = attr(
+        false,
+        componentEl.attr("tr-marquee-scrolldirection")
+      ),
+      scrollScrubSetting = attr(
+        false,
+        componentEl.attr("tr-marquee-scrollscrub")
+      ),
+      moveDistanceSetting = -100,
+      timeScaleSetting = 1,
+      pausedStateSetting = false;
+    if (reverseSetting) moveDistanceSetting = 100;
+    let marqueeTimeline = gsap.timeline({
+      repeat: -1,
+      onReverseComplete: () => marqueeTimeline.progress(1)
+    });
+    if (verticalSetting) {
+      speedSetting = panelEl.first().height() / speedSetting;
+      marqueeTimeline.fromTo(
+        panelEl,
+        { yPercent: 0 },
+        { yPercent: moveDistanceSetting, ease: "none", duration: speedSetting }
+      );
+    } else {
+      speedSetting = panelEl.first().width() / speedSetting;
+      marqueeTimeline.fromTo(
+        panelEl,
+        { xPercent: 0 },
+        { xPercent: moveDistanceSetting, ease: "none", duration: speedSetting }
+      );
+    }
+    let scrubObject = { value: 1 };
+    ScrollTrigger.create({
+      trigger: "body",
+      start: "top top",
+      end: "bottom bottom",
+      onUpdate: (self) => {
+        if (!pausedStateSetting) {
+          if (scrollDirectionSetting && timeScaleSetting !== self.direction) {
+            timeScaleSetting = self.direction;
+            marqueeTimeline.timeScale(self.direction);
+          }
+          if (scrollScrubSetting) {
+            let v = self.getVelocity() * 0.006;
+            v = gsap.utils.clamp(-60, 60, v);
+            let scrubTimeline = gsap.timeline({
+              onUpdate: () => marqueeTimeline.timeScale(scrubObject.value)
+            });
+            scrubTimeline.fromTo(
+              scrubObject,
+              { value: v },
+              { value: timeScaleSetting, duration: 0.5 }
+            );
+          }
+        }
+      }
+    });
+    function pauseMarquee(isPausing) {
+      pausedStateSetting = isPausing;
+      let pauseObject = { value: 1 };
+      let pauseTimeline = gsap.timeline({
+        onUpdate: () => marqueeTimeline.timeScale(pauseObject.value)
+      });
+      if (isPausing) {
+        pauseTimeline.fromTo(
+          pauseObject,
+          { value: timeScaleSetting },
+          { value: 0, duration: 0.5 }
+        );
+        triggerClickEl.addClass("is-paused");
+      } else {
+        pauseTimeline.fromTo(
+          pauseObject,
+          { value: 0 },
+          { value: timeScaleSetting, duration: 0.5 }
+        );
+        triggerClickEl.removeClass("is-paused");
+      }
+    }
+    if (window.matchMedia("(pointer: fine)").matches) {
+      triggerHoverEl.on("mouseenter", () => pauseMarquee(true));
+      triggerHoverEl.on("mouseleave", () => pauseMarquee(false));
+    }
+    triggerClickEl.on("click", function () {
+      !$(this).hasClass("is-paused") ? pauseMarquee(true) : pauseMarquee(false);
+    });
+  });
+});
+
+//Swiper JS Testimonials Section //
+$(".slider-main_component").each(function (index) {
+  let loopMode = false;
+  if ($(this).attr("loop-mode") === "true") {
+    loopMode = true;
+  }
+  let sliderDuration = 300;
+  if ($(this).attr("slider-duration") !== undefined) {
+    sliderDuration = +$(this).attr("slider-duration");
+  }
+  const swiper = new Swiper($(this).find(".swiper")[0], {
+    speed: sliderDuration,
+    loop: loopMode,
+    autoHeight: true,
+    centeredSlides: loopMode,
+    followFinger: true,
+    freeMode: false,
+    slideToClickedSlide: false,
+    slidesPerView: 1,
+    spaceBetween: "4%",
+    rewind: false,
+    mousewheel: {
+      forceToAxis: true
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    },
+    breakpoints: {
+      // mobile landscape
+      480: {
+        slidesPerView: 1,
+        spaceBetween: "4%"
+      },
+      // tablet
+      768: {
+        slidesPerView: 2,
+        spaceBetween: "4%"
+      },
+      // desktop
+      992: {
+        slidesPerView: 2,
+        spaceBetween: "2%"
+      }
+    },
+    pagination: {
+      el: $(this).find(".swiper-bullet-wrapper")[0],
+      bulletActiveClass: "is-active",
+      bulletClass: "swiper-bullet",
+      bulletElement: "button",
+      clickable: true
+    },
+    navigation: {
+      nextEl: $(this).find(".swiper-next")[0],
+      prevEl: $(this).find(".swiper-prev")[0],
+      disabledClass: "is-disabled"
+    },
+    /*scrollbar: {
+      el: $(this).find(".swiper-drag-wrapper")[0],
+      draggable: true,
+      dragClass: "swiper-drag",
+      snapOnRelease: true
+    },*/
+    slideActiveClass: "is-active",
+    slideDuplicateActiveClass: "is-active"
+  });
+});
